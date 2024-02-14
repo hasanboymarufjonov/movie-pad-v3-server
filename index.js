b0e668f5581a3310700cfbe963b022851ac1b6f8
hasanboymarufjonov <therealhasanboy@gmail.com>
2024-02-14 21:53:57 +0500

    .

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(
+  cors({
+    credentials: true,
+    // origin: "http://localhost:5173",
+    origin: ["https://movie-pad-app.vercel.app"],
+  })
+);
+
+app.set("trust proxy", 1);
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://movie-pad-app.vercel.app"];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
 app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
